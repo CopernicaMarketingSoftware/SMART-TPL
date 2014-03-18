5835b3c562ff9d0e731311fcee5f684c108ff4f6
Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
2014-03-18 21:29:54 +0100

    added test case

---
+++ test/main.cpp
@@ -0,0 +1,23 @@
+/**
+ *  Main.cpp
+ * 
+ *  Example program that shows how to use a template
+ * 
+ *  @author Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
+ *  @copyright 2014 Copernica BV
+ */
+#include <smarttpl.h>
+
+/**
+ *  Main function
+ *  @return int
+ */
+int main()
+{
+    // create a template object
+    SmartTpl::Template tpl("template.tpl");
+    
+    // done
+    return 0;
+}
+
