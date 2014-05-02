ecadc2a8398cdf894d64fcda08450c81b1547fa2
Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
2014-05-02 16:24:59 +0200

    iterators no longer have to be stacked inside the handler class

--- src/handler.h
+++ src/handler.h
@@ -32,16 +32,6 @@ private:
     const Data *_data;
 
     /**
-     *  Stack of our iterators
-     *
-     *  This is a stack of active {foreach} loops. The top of the stack
-     *  contains the foreach loop that is now running.
-     * 
-     *  @var std::stack
-     */
-    std::stack<Iterator> _iterators;
-
-    /**
      *  Compare functor necessary for the map
      */
     struct cmp_str
@@ -115,85 +105,6 @@ public:
 
         // didn't find it? get the variable from the data object
         return _data->value(name, size);
-    }
-
-    /**
-     *  Start a new foreach loop and push the initial position of a for loop
-     *  @param  
-     * 
-     * 
-     *  @return Iterator*
-     */
-    void startLoop()
-    {
-        // @todo implementation
-        
-//        _iterator_stack.push(0);
-    }
-
-    /**
-     *  Iterate over a multi value value object
-     *  @param value       The value to iterate over
-     *  @param key         The magic key to assign the next value to
-     *  @param size        The size of the magic key
-     *  @param keyvar      The magic key to assign the member key to
-     *  @param keyvar_size The size of keyvar (may be 0!)
-     *  @return True if we should continue looping, false otherwise
-     */
-    bool iterate(Value *value, const char *key, size_t size, const char* keyvar, size_t keyvar_size)
-    {
-        // @todo implementation
-        return false;
-        
-//        // In case our _iterator_stack is empty just return false
-//        if (_iterator_stack.empty()) return false;
-//
-//        // Retrieve the amount of members in value
-//        size_t len = value->memberCount();
-//
-//        // We can't even iterate over this...
-//        if (len == 0) return false;
-//
-//        // get our iterator
-//        unsigned int &iter = _iterator_stack.top();
-//
-//        // if our iterator goes out of bounds that means that we are done and should clean up
-//        if (iter >= len)
-//        {
-//            // remove our magic value
-//            auto liter = _local_values.find(key);
-//            if (liter != _local_values.end()) _local_values.erase(liter);
-//
-//            // Let's look for our magic key var as well if we used it that is
-//            if (keyvar_size > 0)
-//            {
-//                liter = _local_values.find(keyvar);
-//                if (liter != _local_values.end()) _local_values.erase(liter);
-//            }
-//
-//            // Remove this loop from the stack
-//            _iterator_stack.pop();
-//
-//            // and tell the callback to stop looping
-//            return false;
-//        }
-//        else
-//        {
-//            // assign the next element in the iteration to our magic key
-//            _local_values[key] = value->member(iter);
-//            if (keyvar_size > 0)
-//            {
-//                // The foreach wants a key, so let's try to get one
-//                Value *k = value->key(iter);
-//
-//                // Value isn't required to return a key so let's check if it is nullptr or not
-//                if (k != nullptr) _local_values[keyvar] = k;
-//            }
-//
-//            // increase our iterator
-//            iter++;
-//            return true;
-//        }
     }
 
     /**
