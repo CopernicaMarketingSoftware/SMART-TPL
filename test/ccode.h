3d5b573e7c91593f9f4b49da6df053d506637791
Toon Schoenmakers <toon.schoenmakers@copernica.com>
2014-05-08 11:44:25 +0200

    Added the ability to run the unit tests without compiling templates into shared libraries

--- test/ccode.h
+++ test/ccode.h
@@ -18,50 +18,42 @@
 using namespace SmartTpl;
 using namespace std;
 
-// @todo Add an option to disable this on runtime
-inline void compile(const Template &tpl, bool assert = false)
+/**
+ *  Tries to compile a template to a shared library
+ *  It will compile the template with both gcc and clang, in both cases it'll
+ *  compile it with "-Wall -Werror", so no warnings allowed! If it succesfully
+ *  compiled you can load the shared library by loading the file SHARED_LIBRARY.
+ *  If you want to run the unittests without compiling the templates into shared
+ *  libraries simply run with NO_COMPILE in your enviroment variables
+ *  @todo Maybe add a flag for no compile later on
+ *  @todo Add options to only compile with gcc, or only compile with clang
+ *  @param  tpl The template to actually compile
+ *  @return true if succesfully compiled, false otherwise
+ */
+inline bool compile(const Template &tpl)
 {
+    if (getenv("NO_COMPILE")) return false;
+
+    int gccret = -1;
+    int clangret = -1;
+
     FILE *gccshell = popen("gcc -x c -Wall -Werror -fPIC -shared -o " SHARED_LIBRARY " -", "w");
-    if (assert)
-    {
-        ASSERT_TRUE(gccshell != NULL);
-    }
-    else
-    {
-        EXPECT_TRUE(gccshell != NULL);
-    }
+    EXPECT_TRUE(gccshell != NULL) << strerror(errno);
     if (gccshell)
     {
         fprintf(gccshell, "%s", tpl.compile().c_str());
-        if (assert)
-        {
-            ASSERT_EQ(0, pclose(gccshell)) << "gcc failed to compile this template";
-        }
-        else
-        {
-            EXPECT_EQ(0, pclose(gccshell)) << "gcc failed to compile this template";
-        }
+        gccret = pclose(gccshell);
+        EXPECT_EQ(0, gccret) << "gcc failed to compile this template";
     }
 
     FILE *clangshell = popen("clang -x c -Wall -Werror -fPIC -shared -o " SHARED_LIBRARY " -", "w");
-    if (assert)
-    {
-        ASSERT_TRUE(clangshell != NULL);
-    }
-    else
-    {
-        EXPECT_TRUE(clangshell != NULL);
-    }
+    EXPECT_TRUE(clangshell != NULL) << strerror(errno);
     if (clangshell)
     {
         fprintf(clangshell, "%s", tpl.compile().c_str());
-        if (assert)
-        {
-            ASSERT_EQ(0, pclose(clangshell)) << "clang failed to compile this template";
-        }
-        else
-        {
-            EXPECT_EQ(0, pclose(clangshell)) << "clang failed to compile this template";
-        }
+        clangret = pclose(clangshell);
+        EXPECT_EQ(0, clangret) << "clang failed to compile this template";
     }
+
+    return gccret == 0 || clangret == 0;
 }
--- test/encoding.cpp
+++ test/encoding.cpp
@@ -36,9 +36,10 @@ TEST(Encoding, HtmlToRaw)
     "callbacks->write(userdata,\"&lt;b&gt;This is bold&lt;/b&gt;\",31);\n}\n"
     "const char *mode = \"html\";\n");
     EXPECT_EQ(expectedOutput, tpl.compile());
-    compile(tpl, true); // This will compile the Template into a shared library
-
-    Template library(File(SHARED_LIBRARY)); // Here we load that shared library
-    EXPECT_EQ("&lt;b&gt;This is bold&lt;/b&gt;", library.process()); // We compiled it in html mode so default is html
-    EXPECT_EQ("<b>This is bold</b>", library.process("raw")); // We request raw mode, so it decodes it for us
+    if (compile(tpl)) // This will compile the Template into a shared library
+    {
+        Template library(File(SHARED_LIBRARY)); // Here we load that shared library
+        EXPECT_EQ("&lt;b&gt;This is bold&lt;/b&gt;", library.process()); // We compiled it in html mode so default is html
+        EXPECT_EQ("<b>This is bold</b>", library.process("raw")); // We request raw mode, so it decodes it for us
+    }
 }
