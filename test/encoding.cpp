99fce71697ff013d688596a3dbfe74cc2f09b0df
Toon Schoenmakers <toon.schoenmakers@copernica.com>
2014-05-08 11:22:42 +0200

    Our tests will now compile into shared libraries and we can load these from our tests as well
    Using this we are now testing the C code that is compiled in a certain mode runtime decoding.

--- test/encoding.cpp
+++ test/encoding.cpp
@@ -10,6 +10,8 @@
 #include <gtest/gtest.h>
 #include <smarttpl.h>
 
+#include "ccode.h"
+
 using namespace SmartTpl;
 using namespace std;
 
@@ -21,4 +23,22 @@ TEST(Encoding, Html)
 
     string expectedOutput("&lt;b&gt;This is bold&lt;/b&gt;");
     EXPECT_EQ(expectedOutput, tpl.process("html"));
+}
+
+TEST(Encoding, HtmlToRaw)
+{
+    string input("{mode=html}<b>This is bold</b>");
+    Buffer buffer(input);
+    Template tpl(buffer);
+
+    string expectedOutput("#include <smarttpl/callbacks.h>\n"
+    "void show_template(struct smart_tpl_callbacks *callbacks, void *userdata) {\n"
+    "callbacks->write(userdata,\"&lt;b&gt;This is bold&lt;/b&gt;\",31);\n}\n"
+    "const char *mode = \"html\";\n");
+    EXPECT_EQ(expectedOutput, tpl.compile());
+    compile(tpl, true); // This will compile the Template into a shared library
+
+    Template library(File(SHARED_LIBRARY)); // Here we load that shared library
+    EXPECT_EQ("&lt;b&gt;This is bold&lt;/b&gt;", library.process()); // We compiled it in html mode so default is html
+    EXPECT_EQ("<b>This is bold</b>", library.process("raw")); // We request raw mode, so it decodes it for us
 }
