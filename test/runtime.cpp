6a7f63f2495ee93d760fcd059010add17d557dc4
Toon Schoenmakers <toon.schoenmakers@copernica.com>
2014-05-13 11:46:50 +0200

    Besides running some basic templates using jit in the tests let's also run shared libraries

--- test/runtime.cpp
+++ test/runtime.cpp
@@ -1,7 +1,8 @@
 /**
- *  Jit.cpp
+ *  Runtime.cpp
  *
- *  Jit related tests, these will obivously be executing templates
+ *  Runtime related tests, these tests will be running some basic templates
+ *  with both jit and the compiled shared libraries
  *
  *  @author Toon Schoenmakers <toon.schoenmakers@copernica.com>
  *  @copyright 2014 Copernica BV
@@ -10,10 +11,12 @@
 #include <gtest/gtest.h>
 #include <smarttpl.h>
 
+#include "ccode.h"
+
 using namespace SmartTpl;
 using namespace std;
 
-TEST(Jit, ForEach)
+TEST(RunTime, ForEach)
 {
     string input("{foreach $item in $list}item: {$item}\n{/foreach}");
     Template tpl((Buffer(input)));
@@ -26,9 +29,15 @@ TEST(Jit, ForEach)
 
     string expectedOutput("item: 0\nitem: 1\nitem: 2\nitem: 3\nitem: 4\n");
     EXPECT_EQ(tpl.process(data), expectedOutput);
+
+    if (compile(tpl)) // This will compile the Template into a shared library
+    {
+        Template library(File(SHARED_LIBRARY)); // Here we load that shared library
+        EXPECT_EQ(library.process(data), expectedOutput);
+    }
 }
 
-TEST(Jit, ForEachWithKeys)
+TEST(RunTime, ForEachWithKeys)
 {
     string input("{foreach $map as $key => $value}key: {$key}\nvalue: {$value}{/foreach}");
     Template tpl((Buffer(input)));
@@ -44,31 +53,55 @@ TEST(Jit, ForEachWithKeys)
 
     string expectedOutput("key: 1\nvalue: 1key: 2\nvalue: 2key: 3\nvalue: 3key: 4\nvalue: 4key: 5\nvalue: 5");
     EXPECT_EQ(tpl.process(data), expectedOutput);
+
+    if (compile(tpl)) // This will compile the Template into a shared library
+    {
+        Template library(File(SHARED_LIBRARY)); // Here we load that shared library
+        EXPECT_EQ(library.process(data), expectedOutput);
+    }
 }
 
-TEST(Jit, If)
+TEST(RunTime, If)
 {
     string input("{if true}true{else}false{/if}");
     Template tpl((Buffer(input)));
 
     string expectedOutput("true");
     EXPECT_EQ(tpl.process(), expectedOutput);
+
+    if (compile(tpl)) // This will compile the Template into a shared library
+    {
+        Template library(File(SHARED_LIBRARY)); // Here we load that shared library
+        EXPECT_EQ(library.process(), expectedOutput);
+    }
 }
 
-TEST(Jit, StringComparisonEquals)
+TEST(RunTime, StringComparisonEquals)
 {
     string input("{if \"string1\" == \"string2\"}true{else}false{/if}");
     Template tpl((Buffer(input)));
 
     string expectedOutput("false");
     EXPECT_EQ(tpl.process(), expectedOutput);
+
+    if (compile(tpl)) // This will compile the Template into a shared library
+    {
+        Template library(File(SHARED_LIBRARY)); // Here we load that shared library
+        EXPECT_EQ(library.process(), expectedOutput);
+    }
 }
 
-TEST(Jit, StringComparisonNotEquals)
+TEST(RunTime, StringComparisonNotEquals)
 {
     string input("{if \"string1\" != \"string2\"}true{else}false{/if}");
     Template tpl((Buffer(input)));
 
     string expectedOutput("true");
     EXPECT_EQ(tpl.process(), expectedOutput);
+
+    if (compile(tpl)) // This will compile the Template into a shared library
+    {
+        Template library(File(SHARED_LIBRARY)); // Here we load that shared library
+        EXPECT_EQ(library.process(), expectedOutput);
+    }
 }
