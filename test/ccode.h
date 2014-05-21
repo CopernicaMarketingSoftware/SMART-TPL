d173778aaa60caa03da991622273c1021c0258ba
Toon Schoenmakers <toon.schoenmakers@copernica.com>
2014-05-21 11:01:34 +0200

    Added a few command line flags to the tests to enable/disable compiling with gcc/clang

--- test/ccode.h
+++ test/ccode.h
@@ -18,6 +18,9 @@
 using namespace SmartTpl;
 using namespace std;
 
+extern bool no_clang;
+extern bool no_gcc;
+
 /**
  *  Tries to compile a template to a shared library
  *  It will compile the template with both gcc and clang, in both cases it'll
@@ -25,8 +28,6 @@ using namespace std;
  *  compiled you can load the shared library by loading the file SHARED_LIBRARY.
  *  If you want to run the unittests without compiling the templates into shared
  *  libraries simply run with NO_COMPILE in your enviroment variables
- *  @todo Maybe add a flag for no compile later on
- *  @todo Add options to only compile with gcc, or only compile with clang
  *  @param  tpl The template to actually compile
  *  @return true if succesfully compiled, false otherwise
  */
@@ -37,22 +38,28 @@ inline bool compile(const Template &tpl)
     int gccret = -1;
     int clangret = -1;
 
-    FILE *gccshell = popen("gcc -x c -pipe -Wall -Werror -fPIC -shared -o " SHARED_LIBRARY " -", "w");
-    EXPECT_TRUE(gccshell != NULL) << strerror(errno);
-    if (gccshell)
+    if (no_gcc == false)
     {
-        fprintf(gccshell, "%s", tpl.compile().c_str());
-        gccret = pclose(gccshell);
-        EXPECT_EQ(0, gccret) << "gcc failed to compile this template";
+        FILE *gccshell = popen("gcc -x c -pipe -Wall -Werror -fPIC -shared -o " SHARED_LIBRARY " -", "w");
+        EXPECT_TRUE(gccshell != NULL) << strerror(errno);
+        if (gccshell)
+        {
+            fprintf(gccshell, "%s", tpl.compile().c_str());
+            gccret = pclose(gccshell);
+            EXPECT_EQ(0, gccret) << "gcc failed to compile this template";
+        }
     }
 
-    FILE *clangshell = popen("clang -x c -pipe -Wall -Werror -fPIC -shared -o " SHARED_LIBRARY " -", "w");
-    EXPECT_TRUE(clangshell != NULL) << strerror(errno);
-    if (clangshell)
+    if (no_clang == false)
     {
-        fprintf(clangshell, "%s", tpl.compile().c_str());
-        clangret = pclose(clangshell);
-        EXPECT_EQ(0, clangret) << "clang failed to compile this template";
+        FILE *clangshell = popen("clang -x c -pipe -Wall -Werror -fPIC -shared -o " SHARED_LIBRARY " -", "w");
+        EXPECT_TRUE(clangshell != NULL) << strerror(errno);
+        if (clangshell)
+        {
+            fprintf(clangshell, "%s", tpl.compile().c_str());
+            clangret = pclose(clangshell);
+            EXPECT_EQ(0, clangret) << "clang failed to compile this template";
+        }
     }
 
     return gccret == 0 || clangret == 0;
--- test/main.cpp
+++ test/main.cpp
@@ -8,9 +8,45 @@
  */
 
 #include <gtest/gtest.h>
+#include <getopt.h>
+
+#include "ccode.h"
+
+bool no_clang = false;
+bool no_gcc = false;
+
+static const struct option opts[] = {
+    { "help",              no_argument, 0, 'h' }, // Print a basic help
+    { "no-clang",          no_argument, 0, 'c' }, // Don't compile tests with clang
+    { "no-gcc",            no_argument, 0, 'g' }, // Don't compile tests with gcc
+    { "no-compile",        no_argument, 0, 'C' }, // Don't compile tests at all
+    { 0, 0, 0, 0 }
+};
 
 int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  ::testing::FLAGS_gtest_shuffle = true;
-  return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    ::testing::FLAGS_gtest_shuffle = true;
+    int arg, optindex;
+    while ((arg = getopt_long(argc, argv, "gcCh", opts, &optindex)) != -1)
+    {
+        switch (arg) {
+            case 'c':
+                no_clang = true;
+                break;
+            case 'g':
+                no_gcc = true;
+                break;
+            case 'C':
+                no_clang = true;
+                no_gcc = true;
+                break;
+            case 'h':
+            default:
+                std::cout << "  --no-clang           Don't compile tests with clang" << std::endl
+                          << "  --no-gcc             Don't compile tests with gcc" << std::endl
+                          << "  --no-compile         Don't try to compile tests at all" << std::endl;
+                return EXIT_FAILURE;
+        }
+    }
+    return RUN_ALL_TESTS();
 };
