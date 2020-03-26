1bd1ec1b19e65fd2d65ab0a0aec5157948d87743
David van Erkelens <me@davidvanerkelens.nl>
2020-03-26 11:40:26 +0100

    Extend number format (#19)
    Add custom decimal and thousands separarators

--- src/builtin/number_format.h
+++ src/builtin/number_format.h
@@ -4,8 +4,13 @@
  *  Built-in "|number_format" modifier
  *
  *  @author         David van Erkelens <david.vanerkelens@copernica.com>
- *  @copyright      2019 Copernica BV
+ *  @copyright      2019 - 2020 Copernica BV
  */
+
+/**
+ *  Dependencies
+ */
+#include <locale>
 
 /**
  *  Namespace
@@ -17,6 +22,25 @@ namespace SmartTpl { namespace Internal {
  */
 class NumberFormatModifier : public Modifier
 {
+private:
+    /**
+     *  Struct that works with the std::numpunct facet and can be
+     *  constructed with a char for the decimal and thousands separators 
+     */
+    struct formatter : std::numpunct<char>
+    {
+        // chars for the separators
+        char decimal; char thousand;
+
+        // constructor
+        formatter(char decimal, char thousand) : decimal(decimal), thousand(thousand) {}
+
+        // build in functions for separator formatting
+        char do_thousands_sep()   const { return thousand; } // thousands separator
+        std::string do_grouping() const { return (int) thousand == 0 ? "" : "\3"; } // separate every 3 digits, if we have a separator
+        char do_decimal_point()   const { return decimal; }  // decimal separator
+    };
+
 public:
     /**
      *  Destructor
@@ -31,32 +55,50 @@ public:
      */
     VariantValue modify(const Value &input, const SmartTpl::Parameters &params) override
     {
-        // Convert input to double
-        double original(input.toDouble());
-
-        // make sure we have a parameter containing the format
+        // make sure we have a parameter containing the number of decimals
         if (params.size() < 1) throw NoModification();
 
-        // get the amound of decimals to output
+        // get the amount of decimals to output
         int decimals = params[0].toInteger();
 
-        // buffer to create the printf format
-        char format[10];
+        // get separator variables for decimals and thousands
+        char decimal_separator = '.'; char thousand_separator = (char) 0;
 
-        // format the format
-        sprintf(format, "%%.%if", decimals);
+        // if we have a valid parameter, overwrite decimal separator
+        if (params.size() > 1)
+        {
+            // convert to string
+            auto param = params[1].toString();
 
-        // calculate size of new string
-        size_t size = snprintf(nullptr, 0, format, original);
+            // make sure that we have a character (we could throw if the param is too long?)
+            if (param.size() > 0) decimal_separator = param[0]; 
+        }
 
-        // create buffer
-        char buffer[size + 1];
+        // if we have a valid parameter, overwrite thousands separator
+        if (params.size() > 2)
+        {
+            // convert to string
+            auto param = params[2].toString();
 
-        // create new string
-        sprintf(buffer, format, original);
+            // make sure that we have a character (we could throw if the param is too long?)
+            if (param.size() > 0) thousand_separator = param[0]; 
+        }
+
+        // create stringstream to store formatted number
+        std::stringstream stream;
+
+        // create custom locale for our formatting options
+        std::locale formatting_locale(stream.getloc(), new NumberFormatModifier::formatter(decimal_separator, thousand_separator));
+
+        // set formatting options
+        stream.precision(decimals);
+        stream.imbue(formatting_locale);
+
+        // stream the value (never in scientific format)
+        stream << std::fixed << input.toDouble();
 
         // create object
-        return VariantValue(buffer);
+        return VariantValue(stream.str());
     }
 };
 
